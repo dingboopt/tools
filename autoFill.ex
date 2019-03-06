813f166921de41b28e99373142aca1da89533bfc
dingboopt <dingboopt@gmail.com>
2019-03-06 16:41:58 +0800

    Update autoFill.ex

--- autoFill.ex
+++ autoFill.ex
@@ -1,26 +1,26 @@
 #!/usr/bin/expect
-#set timeout 20 #
+set timeout 200
 set user [lindex $argv 0];
 set passwd [lindex $argv 1];
 spawn git submodule update --init --recursive
 expect "*Username*"
 send "$user\r"
-expect "Passwd*"
+expect "Password*"
 send "$passwd\r"
 expect "*Username*"
 send "$user\r"
-expect "Passwd*"
+expect "Password*"
 send "$passwd\r"
 expect "*Username*"
 send "$user\r"
-expect "Passwd*"
+expect "Password*"
 send "$passwd\r"
 expect "*Username*"
 send "$user\r"
-expect "Passwd*"
+expect "Password*"
 send "$passwd\r"
 expect "*Username*"
 send "$user\r"
-expect "Passwd*"
+expect "Password*"
 send "$passwd\r"
 interact
