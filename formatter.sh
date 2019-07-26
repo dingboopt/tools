53a52e7c91d5925070d476518915cf826d642964
dingboopt <dingboopt@gmail.com>
2019-07-26 10:19:16 +0800

    Update formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -x
 find . -type f -exec sed -i 's/\t/    /g' {} +
+
+//replace a reg expression pattern 
+find . -type f -exec sed -i  's/TRACE_DEBUG(\([^;]*\));/ Log::debug("appId2")<<\1;/g' {} +
 sleep 1
 find . -type f -print0 | xargs -0 perl -pi -e 's/ +$//'
