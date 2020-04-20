8f9edbd6b2971a06eaa431ef093373427575688f
dingboopt <dingboopt@gmail.com>
2020-04-20 11:14:24 +0800

    Update formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -3,6 +3,9 @@ set -x
 find . -type f -exec sed -i 's/\t/    /g' {} +
 
 //replace a reg expression pattern 
-find . -type f -exec sed -i  's/TRACE_DEBUG(\([^;]*\));/ Log::debug("appId2")<<\1;/g' {} +
+find . -name "*.cpp" -type f -exec sed -i  's/TRACE_DEBUG(\([^;]*\));/ Log::debug("appId2")<<\1;/g' {} +
 sleep 1
-find . -type f -print0 | xargs -0 perl -pi -e 's/ +$//'
+find . -name "*.cpp" -type f -print0 | xargs -0 perl -pi -e 's/ +$//'
+
+//find underscore followed lowercase alphabet  _a
+grep -Enr "_[a-z]" dir/ | grep -v  "memset_s\|uint[1-9]*_t\|memcpy_s\|size_t\|include\|g_\|.h" | grep -E "_[a-z]"
