9ce386df3745ad2dc99ec700016675ced91ec8a0
dingboopt <dingboopt@gmail.com>
2022-06-06 11:58:44 +0800

    Update formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -9,3 +9,7 @@ find . -name "*.cpp" -type f -print0 | xargs -0 perl -pi -e 's/ +$//'
 
 //find underscore followed lowercase alphabet  _a
 grep -Enr "_[a-z]" dir/ | grep -v  "memset_s\|uint[1-9]*_t\|memcpy_s\|size_t\|include\|g_\|.h" | grep -E "_[a-z]"
+
+
+//一把改名字
+find .  -type f -exec rename 's/\u8fed\u4ee3\u4e00/\u8fed\u4ee3\u4e8c/'  {} +
